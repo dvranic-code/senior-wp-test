98e361b481b8711fa52877c1ab1c2538f52a3d48
Dejan Rudic Vranic <dejanrudicvranic@gmail.com>
2023-10-16 20:01:49 +0200

    init

---
+++ plugin-name/trunk/simple-voting.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Simple Voting
+ * Description: Simple Voting Plugin
+ * Version: 1.0.0
+ * Author: Dejan Rudic Vranic
+ * Author URI: https://dejan-wp.dev
+ * Text Domain: simple-voting
+ *
+ * @package Simple Voting
+ */
